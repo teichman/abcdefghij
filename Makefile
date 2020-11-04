afa9857196b96e99cbd05545cf6b91dba65d3c55
Alex Teichman <teichman@apple.com>
2020-11-03 22:50:29 -0800

    Adding BaseNum.  Appears to be reproducing Luke's results.

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 
+basenum: basenum.cpp
+	g++ -std=c++14 -O3 $^ -o $@
+
 abcdefghij: abcdefghij.cpp
 	g++ -std=c++14 -O3 $^ -o $@
 
---
+++ basenum.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+int ipow(int base, int exp)
+{
+  int result = 1;
+  for (;;)
+  {
+    if (exp & 1)
+      result *= base;
+    exp >>= 1;
+    if (!exp)
+      break;
+    base *= base;
+  }
+
+  return result;
+}
+
+ostream& operator<<(ostream& out, const vector<uint16_t>& digits)
+{
+  out << digits[0];
+  for (size_t i = 1; i < digits.size(); ++i)
+    out << ", " << digits[i];
+
+  return out;
+}
+
+
+class BaseNum
+{
+public:
+  BaseNum(uint16_t base) : base_(base)
+  {
+    digits_.resize(base);
+    for (size_t i = 0; i < digits_.size(); ++i)
+      digits_[i] = i;
+
+    val_ = digits2val();
+  }
+
+  BaseNum(uint16_t base, const std::vector<uint16_t>& digits) : base_(base), digits_(digits)
+  {
+    val_ = digits2val();
+  }
+
+  bool nextPermutation()
+  {
+    bool flag = std::next_permutation(digits_.begin(), digits_.end());
+    val_ = digits2val();
+    return flag;
+  }
+
+  // Check if this one satisfies the rule, and recursively call on its prefixes.
+  bool isSolution() const
+  {
+    if (digits_.size() == 1)
+      return (digits_[0] != 0);
+    
+    if (val_ % digits_.size() != 0)
+      return false;
+
+    return prefix().isSolution();
+  }
+  
+  uint64_t val() const { return val_; }
+  
+  BaseNum prefix() const
+  {
+    vector<uint16_t> newdigits(digits_.begin(), digits_.begin() + digits_.size() - 1);
+    return BaseNum(base_, newdigits);
+  }
+
+  std::string status(const std::string& prefix = "") const
+  {
+    ostringstream oss;
+    oss << prefix << "Base " << base_ << ": " << digits_ << " (" << val_ << ")  isSolution: " << isSolution();
+    return oss.str();
+  }
+  
+private:
+  uint16_t base_;
+  uint64_t val_;
+  vector<uint16_t> digits_;
+
+  uint64_t digits2val()
+  {
+    uint64_t val = 0;
+    for (size_t i = 0; i < digits_.size(); ++i) {
+      int pow = digits_.size() - i - 1;
+      val += digits_[i] * ipow(base_, pow);
+    }
+    return val;
+  }
+};
+
+
+int main(int argc, char** argv)
+{
+  BaseNum bn(6);
+
+  do {
+    cout << bn.status() << endl;
+  } while (bn.nextPermutation());
+  
+  // cout << bn.status() << endl;
+  // cout << bn.prefix().status() << endl;
+  // cout << bn.prefix().prefix().status() << endl;
+}
